199f1dcd6ce01d98c11a1df06dc505a22ffde79c
Luan Golembiewski <luangolembiewski74@gmail.com>
2022-03-30 19:58:13 -0300

    att

--- Facec.Introducao.WPF/obj/Debug/net5.0-windows/MainWindow.g.cs
+++ Facec.Introducao.WPF/obj/Debug/net5.0-windows/MainWindow.g.cs
@@ -61,7 +61,7 @@ namespace Facec.Introducao.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Facec.Introducao.WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Facec.Introducao.WPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
